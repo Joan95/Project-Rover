8157ce82f48c241a093f9eeabe243669c1f7a534
Canals Mascorda <JCanalsMascorda@lear.com>
2022-02-27 00:56:52 +0100

    Added several Exceptions, modified funcionality of entire program, simplified tasks, discovered one issue (check TODO)

--- main.py
+++ main.py
@@ -10,38 +10,55 @@
 # The possible letters are ‘L’, ‘R’ and ‘M’. ‘L’ and ‘R’ makes the rover spin 90 degrees left or right
 # respectively, without moving from its current spot. ‘M’ means move forward one grid point, and maintain the same
 # heading. Assume that the square directly North from (x, y) is (x, y+1).
+# INPUT:
+# The first line of input is the upper-right coordinates of the plateau, the lower- left coordinates
+# are assumed to be 0,0.
+# The rest of the input is information pertaining to the rovers that have been deployed. Each
+# rover has two lines of input.
+# The first line gives the rover’s position, and the second line is a series of instructions telling the
+# rover how to explore the plateau.
+# The position is made up of two integers and a letter separated by spaces, corresponding to the
+# x and y co-ordinates and the rover’s orientation.
+# Each rover will be finished sequentially, which means that the second rover won’t start to
+# move until the first one has finished moving.
+
+# Python imports
+import copy
+
+# Self imports
+from rover_exceptions import *
 
 # Define CONST variables
 value_zero = 0
 value_one = 1
 cardinal_points = {
-                      'N': {
-                          'Coordinate': 'Y',
-                          'Movement': value_one
-                      },
-                      'E': {
-                          'Coordinate': 'X',
-                          'Movement': value_one
-                      },
-                      'S': {
-                          'Coordinate': 'Y',
-                          'Movement': -value_one
-                      },
-                      'W': {
-                          'Coordinate': 'X',
-                          'Movement': -value_one
-                      }
-                  }
+    'N': {
+        'Coordinate': 'Y',
+        'Movement': value_one
+    },
+    'E': {
+        'Coordinate': 'X',
+        'Movement': value_one
+    },
+    'S': {
+        'Coordinate': 'Y',
+        'Movement': -value_one
+    },
+    'W': {
+        'Coordinate': 'X',
+        'Movement': -value_one
+    }
+}
 # For avoiding Python 2.7 sort dict issue,
 # solved in newer versions of Python
 sorted_cardinal_points_list = ['N', 'E', 'S', 'W']
 
 coordinates_definition = ['X', 'Y']
 set_of_known_movements = {
-                            'L': -value_one,
-                            'R': value_one,
-                            'M': None
-                         }
+    'L': -value_one,
+    'R': value_one,
+    'M': None
+}
 bottom_left_coordinates = [value_zero, value_zero]
 top_right_coordinates = None
 first_line_types = [int, int]
@@ -49,143 +66,90 @@ second_line_types = [int, int, str]
 
 
 def read_rover_parameters(reading_message, types_input_list):
-    """
-    read_rover_paramenters(): A function that will be used in order to read either initial coordinates for the
-     or Rover's
-    position when requested. This function requires two main inputs.
+    # TODO: Change description according new implementations (possible all exceptions fired)
 
-    :param reading_message: Message displayed when asking for input.
-    :param types_input_list: A list of values which are expected to be received whilst reading from keyboard. Those
-    parameters would be used in order to finally crosscheck whether the types are the correct ones or not.
-    :return: correct values read from keyboard, only once they are correct, otherwise it will keep asking for a
-    correct input to be provided.
-    """
-    not_correct_input = True
+    # List that will be returned
     final_values = list()
 
-    # Keep reading until correct data to be fully received
-    while not_correct_input:
-        line_input = str(raw_input("{}: ".format(reading_message)))
+    # Read input, cast it to known type str
+    line_input = str(raw_input("{}: ".format(reading_message)))
 
-        # Check for incomplete data
-        if len(line_input) < (len(types_input_list) + value_one):
-            print '\t[!!]\tLength is incorrect, try it again!'
-        else:
-            # Complete data received, try to get values, check if provided data is full of spaces only
-            values = line_input.split(' ')
+    # Check for incomplete data
+    # TODO: Fix (+ value_one) not working for line_input expected length 5
+    if len(line_input) < (len(types_input_list) + value_one):
+        # Raise Incomplete Data Received Exception
+        raise ExceptionIncompleteDataReceived(len(line_input), len(types_input_list) + value_one)
+    else:
+        # Complete data received, try to get values, check if provided data is full of spaces only
+        values = line_input.split(' ')
+
+        # Process length of values
+        if len(values) > len(types_input_list):
+            # Raise Extra Values Received incorrect input Exception
+            raise ExceptionExtraValuesReceived(len(values), len(types_input_list), [value for value in values])
+
+        # Input must be aligned with types passed over parameter to this function
+        for count, (element, expected_type) in enumerate(zip(values, types_input_list), start=value_zero):
+            # Try forcing cast on each element depending on type provided, if exception ask for new values
             try:
-                # Process length of values
-                if len(values) > len(types_input_list):
-                    print '\t[!!]\tFollowing extra values entered will be discarded: "{}"'.format(', '.join(
-                        [values[count] for count, x in
-                         enumerate(range(len(values) - len(types_input_list)), start=len(types_input_list))]))
-                    # Take exactly very expected first values
-                    values = values[value_zero:len(types_input_list)]
+                # Force the casting on element and wait if accepted or exception is raised
+                expected_type(element)
+                # If here, there is no exception store value in final_value
+                final_values.insert(count, expected_type(element))
 
-                # Number of parameters are correct, start processing type, but first set it as correct,
-                # change it if needed in next loop
-                not_correct_input = not not_correct_input
+                # If int in any case the value provided can be less than 0
+                if expected_type is int and expected_type(element) < value_zero:
+                    # Check that coordinates are not less than [0, 0] coordinates
+                    raise ExceptionValueLessThanZero(count + value_one, element)
 
-                # Init list again, just in case wrong values have been set there
-                final_values = list()
+                # Check whether top_right_coordinates have been set or not, if so, check if over exceeding them
+                if expected_type is int and top_right_coordinates is not None:
+                    # Split if to avoid NoneType error for None in top_right_coordinates list
+                    # Use final_values, for correct type check
+                    if expected_type(element) > top_right_coordinates[count]:
+                        # Check that introduced coordinates are not greater than coordinates introduced
+                        raise ExceptionRoverPlacedOutOfPlane(count + value_one, element,
+                                                             str(top_right_coordinates[count]))
 
-                # must be aligned with types passed over parameter to this function
-                for count, (element, expected_type) in enumerate(zip(values, types_input_list), start=value_zero):
-                    # Try forcing cast on each element depending on type provided, if exception ask for new values
-                    try:
-                        # Force the casting on element and wait if accepted or exception is raised
-                        expected_type(element)
-                        # If here, there is no exception store value in final_value, it might be overwritten if
-                        # it is not correct, but there would be the type accordingly
-                        final_values.insert(count, expected_type(element))
+                # If string check whether it is present in cardinal_points or not
+                if expected_type is str and element.upper() not in cardinal_points.keys():
+                    raise ExceptionOrientationNotKnown(count + value_one, element,
+                                                       ', '.join([x for x in cardinal_points.keys()]))
 
-                        # If int in any case the value provided can be less than 0
-                        if expected_type is int and expected_type(element) < value_zero:
-                            # Check that coordinates are not less than [0, 0] coordinates
-                            print '\t[!!]\tOops... Value in position {} "{}" may be wrong, ' \
-                                  'try it again!\n\t\tNOTE: Value can not be less than 0'.format(count + 1, element)
-                            not_correct_input = True
+            except ValueError:
+                # Raise custom Wrong Type Var exception
+                raise ExceptionWrongTypeVar(count + value_one, element, expected_type, type(element))
 
-                        # Check whether top_right_coordinates have been set or not, if so, check if over exceeding
-                        if expected_type is int and top_right_coordinates is not None:
-                            # Split if to avoid NoneType error for None in top_right_coordinates list
-                            # Use final_values, for correct type check
-                            if expected_type(element) > top_right_coordinates[count]:
-                                # Check that introduced coordinates is not greater than coordinates introduced
-                                print '\t[!!]\tOops... I\'m afraid Rover shouldn\'t be here. ' \
-                                      'Value in position {} "{}" is out of plane, ' \
-                                      'try it again!\n\t\tNOTE: Value must be less than ' \
-                                      '[{}]'.format(count + value_one, element, str(top_right_coordinates[count]))
-                                not_correct_input = True
-
-                        # If string check whether it is present in cardinal_points or not
-                        if expected_type is str and element.upper() not in cardinal_points.keys():
-                            print '\t[!!]\tOops... Value in position {} "{}" may be not the correct one, ' \
-                                  'try it again!\n\t\tNOTE: Possible values: {}'.format(
-                                   count + value_one, element, ', '.join([x for x in cardinal_points.keys()]))
-                            not_correct_input = True
-
-                    except ValueError:
-                        print '\t[!!]\tOops... Value in position {} "{}" may be not the correct one, ' \
-                              'try it again!'.format(count + value_one, element)
-                        not_correct_input = True
-
-            except IndexError:
-                print '\t[!!]\tOops... I\'m afraid Rover should not be here, try it again!'
-                pass
     return final_values
 
 
 def read_set_of_instructions(rover_id):
-    """
-    read_set_of_instructions(): This function will read the set of instructions passed by which would be loaded lately
-    into Rover accordingly. Instructions will be check whether they are known or not. If not, function will ask
-    for new valid values again.
+    # TODO: Change description according new implementations (possible all exceptions fired)
 
-    :param rover_id: For properly printing the valid information.
-    :return: cleaned list of movements to be executed in another function.
-    """
-    not_correct_input = True
+    # Read instruction
+    line_input = str(raw_input("{} is waiting for instruction: ".format(rover_id)))
+
+    # Process line_input make sure that all commands are known, otherwise ask for them again
+    line_input = line_input.replace("\n", "").replace(" ", "")
+
+    # Define list again making sure we are not writing extra values of old requests
     final_set_of_movements = list()
 
-    # Keep reading until correct data to be fully received
-    while not_correct_input:
-        line_input = str(raw_input("{} is waiting for instruction: ".format(rover_id)))
-        print '\n'
-
-        # Process line_input make sure that all commands are known, otherwise ask for them again
-        line_input = line_input.replace("\n", "").replace(" ", "")
-
-        # Take input as valid, check below if some of commands are not present in list of commands known
-        not_correct_input = not not_correct_input
-
-        # Define list again making sure we are not writing extra values of old requests
-        final_set_of_movements = list()
-
-        # Iterate over all commands received
-        for count, letter in enumerate(line_input, start=value_zero):
-            final_set_of_movements.insert(count, letter.upper())
-            # Check whether command is or not known
-            if letter.upper() not in set_of_known_movements.keys():
-                print '\t[!!]\tOops... Rover does not know command in position {} [{}], ' \
-                      'send it again!'.format(count, letter.upper())
-                not_correct_input = True
-
-                # Stop iterating, send the response wait for new set of instructions
-                break
+    # Iterate over all commands received
+    for count, letter in enumerate(line_input, start=value_zero):
+        final_set_of_movements.insert(count, letter.upper())
+        # Check whether command is or not known
+        if letter.upper() not in set_of_known_movements.keys():
+            raise ExceptionInstructionParameterNotKnown(count, letter.upper(),
+                                                        ', '.join([x for x in set_of_known_movements.keys()]))
     return final_set_of_movements
 
 
 def execute_movement(rover_initial_position, requested_set_of_movements):
-    """
-    execute_movement(): This function will take both parameters filled previously with valid data and will attempt
-    performing the movement of Rover.
-    :param rover_initial_position: This is the initial position of the Rover.
-    :param requested_set_of_movements: This is the list of valid movements asked to be performed by Rover.
-    :return:
-    """
+    # TODO: Change description according new implementations (possible all exceptions fired)
+
     # Get initial position of rover, set is a current one
-    rover_current_position = rover_initial_position
+    rover_current_position = copy.deepcopy(rover_initial_position)
 
     # Don't return the complete set of movements till make sure we are not over exceeding the edges of plane
     for command in requested_set_of_movements:
@@ -203,13 +167,16 @@ def execute_movement(rover_initial_position, requested_set_of_movements):
                 sorted_cardinal_points_list)]
 
         # After having performed the action just check whether we are over exceeding the edges of the plane
-        # TODO: Check we are not over exceeding edges of plane before sending last position
         for axis in coordinates_definition:
-            if value_zero < rover_current_position[coordinates_definition.index(axis)] < top_right_coordinates[coordinates_definition.index(axis)]:
+            if value_zero <= rover_current_position[coordinates_definition.index(axis)] <= top_right_coordinates[
+               coordinates_definition.index(axis)]:
+                # Rover is still in the plane
                 pass
             else:
-                break
-
+                # Rover is not in the plane raise exception
+                raise ExceptionRoverAttemptingToExitKnownPlane(rover_current_position, axis,
+                                                               top_right_coordinates[coordinates_definition.index(axis)]
+                                                               )
     return rover_current_position
 
 
@@ -218,12 +185,32 @@ def main():
     main(): Function that will handle all the steps to deploy and move Rovers accordingly.
     :return: Nothing
     """
+    not_correct_input = True
+
     # Read first line, process it accordingly
     global top_right_coordinates
-    top_right_coordinates = read_rover_parameters("Top right coordinates", first_line_types)
+
+    # Keep reading until correct data to be fully received
+    while not_correct_input:
+        try:
+            # Keep reading input until receive correct data
+            top_right_coordinates = read_rover_parameters("Top right coordinates", first_line_types)
+            # Break the loop, input is correct
+            not_correct_input = False
+        except ExceptionIncompleteDataReceived as e:
+            print e
+        except ExceptionExtraValuesReceived as e:
+            print e
+        except ExceptionWrongTypeVar as e:
+            print e
+        except ExceptionValueLessThanZero as e:
+            print e
 
     # Rover's counter
     count = value_zero
+
+    # Clean correct input flag
+    not_correct_input = True
 
     # Keep iterating until end
     # TODO: An end
@@ -231,13 +218,42 @@ def main():
 
     while no_end:
         count += value_one
-        # Read second line, process it accordingly
-        rover_position = read_rover_parameters("\nRover[{}] start position".format(count), second_line_types)
-        requested_set_of_movements = read_set_of_instructions("Rover[{}]".format(count))
 
-        # Execute instruction
-        print "Rover[{}] final position: {}".format(count, ' '.join(
-            str(x) for x in execute_movement(rover_position, requested_set_of_movements)).replace("'", ""))
+        while not_correct_input:
+            try:
+                # Read second line, process it accordingly
+                rover_position = read_rover_parameters("\nRover[{}] start position".format(count), second_line_types)
+                not_correct_input = False
+
+                try:
+                    # Try to read set of instructions
+                    requested_set_of_movements = read_set_of_instructions("Rover[{}]".format(count))
+
+                    # Try to execute received instruction
+                    print "Rover[{}] final position: {}".format(count, ' '.join(
+                        str(x) for x in execute_movement(rover_position, requested_set_of_movements)).replace("'", ""))
+
+                except ExceptionInstructionParameterNotKnown as e:
+                    print e
+
+            except ExceptionIncompleteDataReceived as e:
+                print e
+            except ExceptionExtraValuesReceived as e:
+                print e
+            except ExceptionWrongTypeVar as e:
+                print e
+            except ExceptionValueLessThanZero as e:
+                print e
+            except ExceptionRoverPlacedOutOfPlane as e:
+                print '\t[!!]\tOops... Rover, the fearless explorer, should better not be placed there.'
+                print e
+            except ExceptionOrientationNotKnown as e:
+                print e
+            except ExceptionRoverAttemptingToExitKnownPlane as e:
+                print "\t[!!]\tOops... We do know Rover is a fearless explorer but it is better for its own " \
+                      "security not letting it going through there!"
+                print e
+                print "\t\t\t[Execution not started, execution aborted]"
     pass
 
 
---
+++ rover_exceptions.py
@@ -0,0 +1,126 @@
+import sys
+
+# Define Python Version for 3.0 or greater
+python_version_3 = 3
+try_it_again = "\n\t\tTry it again!"
+
+
+class ExceptionIncompleteDataReceived(Exception):
+    """
+
+    """
+    def __init__(self, length_received, length_expected, default_message="Length of message received is incorrect"):
+        self.length_received = length_received
+        self.length_expected = length_expected
+        self.message = "\t\t[ERROR] - {}. Message length received: {}. " \
+                       "Message length expected: {}.{}".format(default_message, self.length_received,
+                                                               self.length_expected, try_it_again)
+
+        if sys.version_info[0] < python_version_3:
+            # Python 2.7 or less
+            super(ExceptionIncompleteDataReceived, self).__init__(self.message)
+        else:
+            # Python 3
+            # super().__init__(self.message)
+            # TODO: Find exception launcher for Python 3
+            pass
+
+
+class ExceptionExtraValuesReceived(Exception):
+    def __init__(self, length_received, length_expected, extra_values, default_message="Extra values received"):
+        self.length_received = length_received
+        self.length_expected = length_expected
+        self.extra_values = extra_values
+        self.message = "\t\t[ERROR] - {}. Message length received: {}. " \
+                       "Message length expected: {}. Message: {}.{}".format(default_message, self.length_received,
+                                                                            self.length_expected, self.extra_values,
+                                                                            try_it_again)
+
+        if sys.version_info[0] < python_version_3:
+            super(ExceptionExtraValuesReceived, self).__init__(self.message)
+
+
+class ExceptionWrongTypeVar(Exception):
+    def __init__(self, error_position_found, affected_element, expected_type, current_type,
+                 default_message="Wrong variable type received"):
+        self.error_position_found = error_position_found
+        self.affected_element = affected_element
+        self.expected_type = expected_type
+        self.current_type = current_type
+        self.message = "\t\t[ERROR] - {}. Variable affected \"{}\" found in position [{}] is type \"{}\" " \
+                       "and \"{}\" expected. {}".format(default_message, self.affected_element,
+                                                        self.error_position_found, self.current_type,
+                                                        self.expected_type, try_it_again)
+
+        if sys.version_info[0] < python_version_3:
+            super(ExceptionWrongTypeVar, self).__init__(self.message)
+
+
+class ExceptionValueLessThanZero(Exception):
+    def __init__(self, error_position_found, affected_element, default_message="Value can not be less than zero"):
+        self.error_position_found = error_position_found
+        self.affected_element = affected_element
+        self.message = "\t\t[ERROR] - {}. Variable affected \"{}\" " \
+                       "is parameter [{}]. {}".format(default_message, self.affected_element, self.error_position_found,
+                                                      try_it_again)
+
+        if sys.version_info[0] < python_version_3:
+            super(ExceptionValueLessThanZero, self).__init__(self.message)
+
+
+class ExceptionRoverPlacedOutOfPlane(Exception):
+    def __init__(self, error_position_found, affected_element, delimiter,
+                 default_message="Value can not be higher than Plane Axis defined"):
+        self.error_position_found = error_position_found
+        self.affected_element = affected_element
+        self.delimiter = delimiter
+        self.message = "\t\t[ERROR] - {}. Variable affected \"{}\" " \
+                       "is parameter [{}] and can not be higher than Axis edge previously defined \"{}\". " \
+                       "{}".format(default_message, self.affected_element, self.error_position_found, self.delimiter,
+                                   try_it_again)
+
+        if sys.version_info[0] < python_version_3:
+            super(ExceptionRoverPlacedOutOfPlane, self).__init__(self.message)
+
+
+class ExceptionOrientationNotKnown(Exception):
+    def __init__(self, error_position_found, affected_element, list_of_possible_values,
+                 default_message="Wrong Orientation, parameter not known"):
+        self.error_position_found = error_position_found
+        self.affected_element = affected_element
+        self.list_of_possible_values = list_of_possible_values
+        self.message = "\t\t[ERROR] - {}. Variable affected \"{}\" " \
+                       "is parameter [{}] and it is not present in known orientations [{}]. " \
+                       "{}".format(default_message, self.affected_element, self.error_position_found,
+                                   self.list_of_possible_values, try_it_again)
+        if sys.version_info[0] < python_version_3:
+            super(ExceptionOrientationNotKnown, self).__init__(self.message)
+
+
+class ExceptionInstructionParameterNotKnown(Exception):
+    def __init__(self, error_position_found, instruction_not_known, list_of_known_instructions,
+                 default_message="Instruction not known"):
+        self.instruction_not_known = instruction_not_known
+        self.error_position_found = error_position_found
+        self.list_of_known_instructions = list_of_known_instructions
+        self.message = "\t\t[ERROR] - {}. Instruction \"{}\" found at position [{}] is not known. " \
+                       "List of possible instructions: [{}].{}".format(default_message, self.instruction_not_known,
+                                                                       self.error_position_found,
+                                                                       self.list_of_known_instructions, try_it_again)
+        if sys.version_info[0] < python_version_3:
+            super(ExceptionInstructionParameterNotKnown, self).__init__(self.message)
+
+
+class ExceptionRoverAttemptingToExitKnownPlane(Exception):
+    def __init__(self, first_wrong_position, axis_broken, axis_top_value, default_message=
+                 "Pre-processed instructions may result in Rover exceeding plane edge"):
+        self.first_wrong_position = first_wrong_position
+        self.axis_broken = axis_broken
+        self.axis_top_value = axis_top_value
+        self.message = "\t\t[ERROR] - {}. Edge broken at point [{}], " \
+                       "Axis {}, maximum value for it is \"{}\". {}".format(default_message, self.first_wrong_position,
+                                                                            self.axis_broken, self.axis_top_value,
+                                                                            try_it_again)
+
+        if sys.version_info[0] < python_version_3:
+            super(ExceptionRoverAttemptingToExitKnownPlane, self).__init__(self.message)
